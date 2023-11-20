8f3384851ce7cce7d55d843b30d853c18d0f217a
hemanth <hemanthtadikonda29@gmail.com>
2023-11-20 14:52:23 +0530

    pc - "rabbitmq"

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
   name_prefix = "${var.env}-rabbitmq"
-  tags = merge( var.tags , {tf-module = "rabbitmq"}, {env = var.env})
+  tags        = merge( var.tags,{ tf-module = "rabbitmq" },{ env = var.env })
 }
--- main.tf
+++ main.tf
@@ -2,38 +2,38 @@ resource "aws_security_group" "main" {
   name        = "${local.name_prefix}-sg"
   description = "${local.name_prefix}-sg"
   vpc_id      = var.vpc_id
-  tags = merge(local.tags , {Name = "${local.name_prefix}-sg" })
+  tags        = merge(local.tags,{ Name = "${local.name_prefix}-sg" })
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = var.ssh_ingress_cidr
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ssh_ingress_cidr
   }
   ingress {
-    description      = "RABBITMQ"
-    from_port        = 5672
-    to_port          = 5672
-    protocol         = "tcp"
-    cidr_blocks      = var.sg_ingress_cidr
+    description = "RABBITMQ"
+    from_port   = 5672
+    to_port     = 5672
+    protocol    = "tcp"
+    cidr_blocks = var.sg_ingress_cidr
   }
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
   }
 }
 
 resource "aws_instance" "main" {
-  ami           = data.aws_ami.ami.id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id[0]
-  vpc_security_group_ids = [ aws_security_group.main.id]
-  user_data     = file("${path.module}/userdata.sh")
-  tags = merge(local.tags , {Name = "${local.name_prefix}-inst-${count.index + 1}" })
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id[ 0 ]
+  vpc_security_group_ids = [ aws_security_group.main.id ]
+  user_data              = file("${path.module}/userdata.sh")
+  tags                   = merge(local.tags,{ Name = "${local.name_prefix}-inst" })
 
 }
 
@@ -42,5 +42,5 @@ resource "aws_route53_record" "www" {
   name    = local.name_prefix
   type    = "A"
   ttl     = 30
-  records = [aws_instance.main.private_ip]
+  records = [ aws_instance.main.private_ip ]
 }

6815997a495afaff7801cad123e6a2f13f3f33d2
hemanth <hemanthtadikonda29@gmail.com>
2023-11-20 15:00:03 +0530

    pc - "rabbitmq"

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install ansible -y
-ansible-pull -i localhost, -U
+yum install ansible -y &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/hemanthtadikonda/rb-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
